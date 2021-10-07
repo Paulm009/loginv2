4e991f975a752d5a9f586f3bee224a403963b0a1
Paulm009 <poolpos8@gmail.com>
2021-10-06 20:40:37 -0400

    version1

---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="15">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="15">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="tecenter display-2 teal--text text--accent-3"
+                        >
+                          Bienvenido a Estuqueria Cayara S.R.L
+                        </h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="blue" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="red" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">
+                          Ingrese su cuenta para el ingreso
+                        </h4>
+                        <v-form>
+                          <v-text-field
+                            label="Cuenta"
+                            name="Cuenta"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Contraseña"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                        <h3 class="text-center mt-4">
+                          Olvidaste tu contraseña ?
+                        </h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="teal accent-3" dark
+                          >INGRESAR</v-btn
+                        >
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
